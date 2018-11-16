4f6cc29c148d9d1138962fba40acaa0510392c23
Witold Wiecek <ucrc.lxl@gmail.com>
2018-11-16 15:17:19 +0000

    increase variance in automatically decided prior

--- R/auto_prior.R
+++ R/auto_prior.R
@@ -8,7 +8,7 @@ auto_prior <- function(data, stan_data, model, outcome = "outcome",
   message("Automatically setting prior values:")
 
   if(model %in% c("rubin")) {
-    prior_list[["prior_upper_sigma_tau"]] <- 5*var(data$tau)
+    prior_list[["prior_upper_sigma_tau"]] <- 10*var(data$tau)
     message(paste0("* sigma_tau ~ Uniform(0, ",
                    round(prior_list[["prior_upper_sigma_tau"]], 2), ")"))
     prior_list[["prior_tau_mean"]] <- 0
@@ -17,14 +17,14 @@ auto_prior <- function(data, stan_data, model, outcome = "outcome",
   }
   if(model == "mutau") {
     # Remember, first row is always mu (baseline), second row is tau (effect)
-    prior_list[["prior_upper_sigma_tau"]] <- c(5*var(data$mu), 5*var(data$tau))
+    prior_list[["prior_upper_sigma_tau"]] <- c(10*sd(data$mu), 10*sd(data$tau))^2
     message(paste0("* sigma_mu ~ Uniform(0, ",
-                   round(prior_list[["prior_upper_sigma_tau"]][1], 2), ")"))
+                   round(sqrt(prior_list[["prior_upper_sigma_tau"]][1]), 2), ")"))
     message(paste0("* sigma_tau ~ Uniform(0, ",
-                   round(prior_list[["prior_upper_sigma_tau"]][2], 2), ")"))
+                   round(sqrt(prior_list[["prior_upper_sigma_tau"]][2]), 2), ")"))
     prior_list[["prior_tau_mean"]] <- c(0,0)
     prior_list[["prior_tau_scale"]] <- 1000*diag(2)
-    message(paste0("(mu, tau) ~ Normal([0,0], (1000^2)*Id_2)"))
+    message(paste0("mean priors: (mu, tau) ~ Normal([0,0], (1000^2)*Id_2)"))
   }
   if(model == "full") {
     # empirical variance of outcome:
@@ -32,7 +32,7 @@ auto_prior <- function(data, stan_data, model, outcome = "outcome",
     message(paste("* Prior variance set to 5 times the observed variance in outcome."))
     prior_list[["P"]] <- 2
     prior_list[["mutau_prior_mean"]]  <- rep(0, prior_list$P)
-    prior_list[["mutau_prior_sigma"]] <- 5*vhat*diag(prior_list$P)
+    prior_list[["mutau_prior_sigma"]] <- 10*vhat*diag(prior_list$P)
   }
   if(model == "quantiles") {
     prior_list[["prior_dispersion_on_beta_0"]] <- 1000*diag(stan_data$N)
