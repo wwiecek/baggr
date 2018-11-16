98f0e78c8ff36338eb7e7e4db7dc84b83142b8c9
Witold Wiecek <ucrc.lxl@gmail.com>
2018-11-16 15:49:05 +0000

    pooling bug (var vs SD confusion)

--- R/pooling_metrics.R
+++ R/pooling_metrics.R
@@ -35,7 +35,7 @@ pooling <- function(bg, metric = "gelman-hill", summary = TRUE) {
 
     if(bg$model == "mutau"){
       sigma_k <- bg$data$se.tau
-      sigma_tau <- rstan::extract(bg$fit, pars = "sigma_tau")[[1]][,2,2]
+      sigma_tau <- sqrt(rstan::extract(bg$fit, pars = "sigma_tau")[[1]][,2,2])
     }
     if(bg$model == "rubin"){
       sigma_k <- bg$data$se
@@ -49,7 +49,7 @@ pooling <- function(bg, metric = "gelman-hill", summary = TRUE) {
     # note that we use a point estimate for sigma_k, so it's not fully Bayesian
     # but in the end we usually report a point estimate so this is acceptable for now
     sigma_k <- study_effects(bg, summary = TRUE)[, "sd", 1]
-    sigma_tau <- rstan::extract(bg$fit, pars = "sigma_mutau[2,2]")[[1]]
+    sigma_tau <- sqrt(rstan::extract(bg$fit, pars = "sigma_mutau[2,2]")[[1]])
     # we add 3rd dimension to allow more than 1 parameter (in the future), e.g. Var
     ret <- array(0, dim = c(length(sigma_tau), bg$n_groups, 1))
     ret[,,1] <- sapply(sigma_k, function(sigma) sigma^2 / (sigma^2 + sigma_tau^2))
