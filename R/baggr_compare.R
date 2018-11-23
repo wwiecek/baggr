80bbedb5e717a51482e7021364ea6dafd95a2e2b
Witold Wiecek <ucrc.lxl@gmail.com>
2018-11-23 12:59:35 +0000

    model comparison also returns full pooling model

--- R/baggr_compare.R
+++ R/baggr_compare.R
@@ -6,8 +6,6 @@
 #'            function, but with `pooling = ...` omitted.
 #'            In the latter case 3 models will be run, with
 #'            pooling set to `none`, `partial` and `full`.
-#' @param compare can be 'groups' (compare different groups in each model),
-#'                or 'ate' (compare aggregate treatment effects only)
 #' @param style What kind of plot to display - see options for `plot.baggr`
 #' @param arrange If `single`, generate a single plot, if `grid`
 #'                display multiple plots side-by-side.
@@ -17,7 +15,6 @@
 #' @author Witold Wiecek
 #' @importFrom gridExtra grid.arrange
 #' @importFrom dplyr bind_rows
-#' @importFrom dplyr mutate
 #' @importFrom tidyr gather
 #' @import ggplot2
 #' @export
@@ -25,21 +22,23 @@
 
 baggr_compare <- function(...,
                           compare = "group",
-                          style = "areas",
+                          style   = "areas",
                           arrange = "single") {
   l <- list(...)
   if(length(l) == 0)
-    stop("Provide baggr models or model specification.")
+    stop("Must provide baggr models or model specification.")
   if(all(unlist(lapply(l, class)) == "baggr")) {
     models <- l
   } else {
     if("pooling" %in% names(l))
       stop("Can't run the model comparison with pooling setting
             already set to a particular value.")
-    models <- lapply(list("none", "partial"), function(pool){
+
+    # This is where the models are run:
+    models <- lapply(list("none", "partial", "full"), function(pool){
       try(do.call(baggr, c(l, "pooling" = pool)))
     })
-    names(models) <- c("none", "partial")
+    names(models) <- c("none", "partial", "full")
   }
   if(arrange == "grid") {
     plots <- lapply(models, plot, style = style)
@@ -47,14 +46,30 @@ baggr_compare <- function(...,
   }
   if(arrange == "single") {
     # Note: pipe operators are not used here for compatibility and
-    # to reduce dependencies
-    df <- dplyr::bind_rows(
-      lapply(models, function(x) as.data.frame(study_effects(x, summary = TRUE))),
-      .id = "model")
-    df <- dplyr::mutate(df, parameter = factor(parameter, levels = unique(parameter[order(median)])))
-    ggplot2::ggplot(df, aes(x = parameter, y = median, ymin = lci, ymax = uci,
-                   group = interaction(model),
-                   color = model)) +
+    # to reduce dependencies but i still went for dplyr as it's so much simpler
+    df_groups <- dplyr::bind_rows(
+      lapply(models, function(x) {
+        # will need to be modified for quantiles models case:
+        m <- as.data.frame(study_effects(x, summary = TRUE)[,,1])
+        m$parameter <- rownames(m)
+        m
+      }), .id = "model")
+    df_groups$parameter <- factor(df_groups$parameter,
+                                  levels = unique(df_groups$parameter[order(df_groups$median)]))
+
+    # now treatment effect:
+    # df_trt <- dplyr::bind_rows(
+    #   lapply(models, function(x) {
+    #     m <- mint(treatment_effect(x)$tau)
+    #     m$parameter <- rownames(m)
+    #     m
+    #   }), .id = "model")
+
+    # df <- rbind(df_groups, df_trt)
+    df <- df_groups
+    comparison_plot <- ggplot2::ggplot(df, aes(x = parameter, y = median, ymin = lci, ymax = uci,
+                            group = interaction(model),
+                            color = model)) +
       geom_point(size = 2, position=position_dodge(width=0.5)) +
       # geom_jitter(size = 2) +
       geom_errorbar(size = 1.2, width = 0, alpha = .5, position=position_dodge(width=0.5)) +
@@ -62,5 +77,9 @@ baggr_compare <- function(...,
       labs(x = "", y = "Treatment effect (95% interval)",
            title = "Comparison of treatment effects by group")
   }
-}
 
+  # render the plot (for now not saved)
+  plot(comparison_plot)
+
+  return(models)
+}
