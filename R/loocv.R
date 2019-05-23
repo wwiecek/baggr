e461d373743e9e98ce0aa79515b8d7d8fc418426
Witold Wiecek <20645798+wwiecek@users.noreply.github.com>
2019-05-23 22:59:30 +0100

    LOO CV works with individual level data

--- R/loocv.R
+++ R/loocv.R
@@ -1,25 +1,37 @@
 #' Leave one out cross-validation for \code{baggr} models
 #'
-#' @param data Input data frame - same as for \code{baggr()} function.
+#' Creates $k$ `baggr` models by leaving out one group at the time
+#' and calculating log predictive density \eqn{lpd_{k}}
+#' for that group (see Gelman _et al_). Returned value is a \eqn{-2\sum lpd_{k}}.
+#' Inputs to this function are same as to [baggr], with additional
+#' option to return individual models.
+#'
+#'
+#' @param data Input data frame - same as for [baggr] function.
 #' @param return_models logical; if FALSE, summary statistics will be returned and the
 #'                      models discarded;
 #'                      if TRUE, a list of models will be returned alongside summaries
-#' @param ... Additional arguments passed to \code{baggr()}.
-#' @return A data.frame of results and list of baggr models
+#' @param ... Additional arguments passed to [baggr].
+#' @return log predictive density value, an object of class `baggr_cv`;
+#' full model, prior values and _lpd_ of each model are also returned
+#' these can be examined by using `attributes()` function
 #'
 #' @details
-#' For each model we calculate log predictive density l.p.d. (TBC)
+#' __It is recommended to set `mc.cores` option before running `loocv`, e.g. `options(mc.cores = 4)`.__
+#'
+#' __This section is under construction__
 #'
 #' @examples
 #' #even simple examples may take a long moment
-#' loocv(schools, pooling = "partial")
-#' loocv(schools, pooling = "full")
+#' cv <- loocv(schools, pooling = "partial")
+#' print(cv) #returns the lpd value
+#' attributes(cv) #more information is included in the object
 #'
 #' @author Witold Wiecek
 #' @references Gelman A, Hwang J, Vehtari A.
 #'             Understanding predictive information criteria for Bayesian models.
 #'             Statistics and Computing. 2014 Nov 24(6):997-1016.
-#'             \href{http://www.stat.columbia.edu/~gelman/research/published/waic_understand3.pdf}{(PDF link)}
+#'             [PDF link.](http://www.stat.columbia.edu/~gelman/research/published/waic_understand3.pdf)
 #'
 #' @export
 #' @importFrom utils txtProgressBar
@@ -27,20 +39,26 @@
 #'
 
 loocv <- function(data, return_models = FALSE, ...) {
-  K <- nrow(data)
-  if(K > 50)
-    message(paste("Large number of groups - note that the
-                   model will be repeated", K, "times"))
-
   # Set prior, if not specified by the user
   args <- list(...)
   # Stop if pooling == "none"
   if(!is.null(args[["pooling"]]) && (args[["pooling"]] == "none"))
     stop("For a model with no pooling LOO CV doesn't exist.")
 
+  # Set parallel up...
+  if(is.null(getOption("mc.cores"))){
+    cat(paste0("loocv() automatically set options(mc.cores = parallel::detectCores()) \n"))
+    temp_cores <- TRUE
+    options(mc.cores = parallel::detectCores())
+  } else {
+    temp_cores <- FALSE
+  }
 
   # Model with all of data:
-  full_fit <- baggr(data, ...)
+  full_fit <- try(baggr(data, ...))
+  if(class(full_fit) == "try-error")
+    stop("Inference failed for the full model")
+
 
   # Prepare the arguments
   args[["data"]] <- data
@@ -50,21 +68,52 @@ loocv <- function(data, return_models = FALSE, ...) {
     args[["prior"]] <- full_fit$prior
   }
 
+  # Determine number and names of groups
+  if(args[["model"]] == "full") {
+    if(!is.null(args[["group"]]))
+      group_col <- args[["group"]]
+    else
+      group_col <- "group"
+    group_names <- unique(data[[group_col]])
+    K <- length(group_names)
+  } else {
+    K <- nrow(data)
+  }
+  if(K > 50){
+    message(paste("Large number of groups - note that the
+                   model will be repeated", K, "times"))
+  } else {
+    message(paste0("Repeating baggr() for ", K, " separate models"))
+  }
+
+
   # LOO CV models
   cat("\n")
   pb <- utils::txtProgressBar(style = 3)
-  # should sink() Stan print()'s?
   kfits <- lapply(as.list(1:K), function(i) {
-    # baggr(data = data[-i,], test_data = data[i,], ...)
-    args$data <- data[-i,]
-    args$test_data <- data[i,]
+    if(args[["model"]] == "full") {
+      args$data      <- data[data[[group_col]] != group_names[i], ]
+      args$test_data <- data[data[[group_col]] == group_names[i], ]
+    } else {
+      args$data      <- data[-i,]
+      args$test_data <- data[i,]
+    }
+
     utils::setTxtProgressBar(pb, (i-1)/K)
-    do.call(baggr, args)
+
+    # Run baggr models but sinking their outputs
+    res <- try(do.call(baggr, args))
+    if(class(res) == "try-error") {
+      stop(paste0("Inference failed for model number ", i, " out of ", K))
+    } else {
+      return(res)
+    }
   })
+  utils::setTxtProgressBar(pb, 1)
   close(pb)
 
   tau_estimate <-
-    lapply(kfits, function(x) apply(as.matrix(x$fit, "tau"), 2, mean))
+    lapply(kfits, function(x) mean(treatment_effect(x)[[1]]))
   loglik <-
     lapply(kfits, function(x) apply(as.matrix(x$fit, "logpd"), 2, mean))
 
@@ -78,11 +127,17 @@ loocv <- function(data, return_models = FALSE, ...) {
   )
   if(return_models)
     out$models <- kfits
-
   class(out) <- "baggr_cv"
+
+  if(temp_cores)
+    options(mc.cores = NULL)
+
   return(out)
 }
 
+
+
+#' @export
 print.baggr_cv <- function(x, ...) {
   attributes(x) <- NULL
   print(x)
