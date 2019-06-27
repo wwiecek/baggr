9e3c6c6226ddff66e98c38842f4c4d999e8b2f8a
rmeager <rachael.meager@gmail.com>
2019-06-27 13:24:09 +0300

    wrote data description bits

--- R/microcredit.R
+++ R/microcredit.R
@@ -1,24 +1,38 @@
 #' 7 studies on effect of microcredit supply
 #'
-#' Rachael will write this up.
+#' This dataframe contains the data used in Meager (2019) to estimate hierarchical models on the data from 7 randomized controlled trials of expanding access to microcredit.
+#' The columns include the group indicator which gives the name of the lead author on each of the respective studies, the value of the 6 outcome variables of most interest (consumer durables spending, business expenditures, business profit, business revenues, temptation goods spending and consumption spending) all of which are standardised to USD PPP in 2009 dollars per two weeks (these are flow variables), and finally a treatment assignment status indicator.
+#' The dataset has not otherwise been cleaned and therefore includes NAs and other issues common to real data.
+#' For more information on how and why these variables were chosen and standardised, see Meager (2019) or consult the associated code repository which includes the standardisation scripts, which can be found at https://bitbucket.org/rmeager/aggregate-average-impacts-microcredit/src/master/
 #'
 #' @format A data frame with 40267 rows, 7 study idenfitifiers and 7 outcomes
 "microcredit"
+#' @references Meager, Rachael (2019) Understanding the average impact of microcredit expansions: A Bayesian hierarchical analysis of seven randomized experiments. American Economic Journal: Applied Economics, 11(1), 57-91.
+
 
 
 #' Simplified version of the microcredit dataset.
 #'
+#' This dataframe contains the data used in Meager (2019) to estimate hierarchical models on the data from 7 randomized controlled trials of expanding access to microcredit.
+#' The columns include the group indicator which gives the name of the lead author on each of the respective studies, the value of the household consumer durables spending standardised to USD PPP in 2009 dollars per two weeks (these are flow variables), and finally a treatment assignment status indicator.
+#' The dataset has not otherwise been cleaned and therefore includes NAs and other issues common to real data.
+#' For more information on how and why these variables were chosen and standardised, see Meager (2019) or consult the associated code repository which includes the standardisation scripts, which can be found at https://bitbucket.org/rmeager/aggregate-average-impacts-microcredit/src/master/
+
 #'  Includes only complete cases and only the consumer durables outcome variable.
 #'
 #' @format A data frame with 14224 rows, 7 study idenfitifiers and 1 outcome
 "microcredit_simplified"
+#' @references Meager, Rachael (2019) Understanding the average impact of microcredit expansions: A Bayesian hierarchical analysis of seven randomized experiments. American Economic Journal: Applied Economics, 11(1), 57-91.
 
 
 #' 8 schools example
 #'
 #' A classic example of aggregate level continuous data in Bayesian hierarchical modelling.
-#' See Gelman et al, Chapter 5, for context and applied example.
+#' This dataframe contains a column of estimated treatment effects of an SAT prep program implemented in 8 different schools in the US, and a column of estimated standard errors.
+#' See Gelman et al (1995), Chapter 5, for context and applied example.
 #'
 #' @references Gelman, Andrew, John B. Carlin, Hal S. Stern, and Donald B. Rubin.
 #' Bayesian Data Analysis. Taylor & Francis, 1995.
+#'
 "schools"
+
