8cd334b0182fc3fdc67e005035f12a9830ec2943
Witold Wiecek <20645798+wwiecek@users.noreply.github.com>
2019-10-28 18:57:00 +0000

    documentation updates (RM)

--- R/prior_dist.R
+++ R/prior_dist.R
@@ -3,12 +3,13 @@
 #' @name priors
 #' @description This page provides a list of all available distributions
 #' that can be used to specify priors in [baggr()]. These convenience functions
-#' are designed only to let user write the priors in the most "natural" way. Apart from
-#' passing on the arguments, their only other role is rudimentary check if the distribution
-#' is specified correctly.
+#' are designed to allow the user to write the priors in the most "natural" way when
+#' implementing them in baggr. Apart from
+#' passing on the arguments, their only other role is to perform a rudimentary check
+#' if the distribution is specified correctly.
 #'
-#' @param location Mean for normal, multivariate normal (in which case `location` is a vector)
-#'                 and for Cauchy distributions
+#' @param location Mean for normal and multivariate normal (in which case `location` is a vector),
+#'                 and median for Cauchy distributions
 #' @param scale SD for Normal, scale for Cauchy
 #' @param Sigma Variance-covariance matrix for multivariate normal.
 #' @param lower Lower bound for Uniform
@@ -16,6 +17,50 @@
 #' @param shape Shape parameter for LKJ
 #' @param order Order of LKJ matrix (typically it does not need to be specified,
 #'        as it is inferred directly in the model)
+#'
+#' @details
+#'
+#' The prior choice in [baggr] is always done via 3 distinct arguments: `prior_hypermean`,
+#' `prior_hypersd`, and `prior_hypercor`.
+#'
+#' These respectively refer to the priors on the average of the effects across
+#' the groups (hypermean), the standard deviation of the effects across the groups
+#' (hypersd), and the correlation in the distribution of parameters across groups
+#' when the model allows multivariate shrinkage (say on control group means and effects).
+#'
+#' Notation for priors is "plain-text", in that you can write the distributions as
+#' `normal(5,10)`, `uniform(0,100)` etc.
+#' As with any other argument one has the option to simply input the prior directly,
+#' e.g. `prior_hypermean = normal(0,1)` , or by creating a named list of custom priors
+#' and then inputting the list to the argument `priors`.
+#' See the examples below for more.
+#'
+#' Different parameters admit different priors:
+#'
+#' * `prior_hypermean` will take `"normal"`, `"uniform"` and `"cauchy"` input for a scalar mean.
+#'    For a vector mean, it will take any of these arguments and apply them independently to
+#'    each component of the vector, or it can also take a `"multinormal"` argument
+#'    (see the example below).
+#' * `prior_hypersd` will take `"normal"` and `"uniform"`
+#' * `prior_hypercor` allows `"lkj"` input
+#'
+#'
+#' @author Witold Wiecek, Rachael Meager
+#'
+#' @examples
+#' # change the priors for 8 schools:
+#' baggr(schools, model = "rubin", pooling = "partial",
+#'       prior_hypermean = normal(5,5),
+#'       prior_hypersd = normal(0,20))
+#'
+#' \donttest{
+#' # passing priors as a list
+#' custom_priors <- list(hypercor = lkj(1), hypersd = normal(0,10),
+#'                       hypermean = multinormal(c(0,0),matrix(c(10,3,3,10),2,2)))
+#' baggr(microcredit_summary_data, model = "mutau",
+#'       pooling = "partial", prior = custom_priors)
+#' }
+
 NULL
 
 check_scalar <- function(x) {
