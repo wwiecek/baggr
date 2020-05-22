a1496be6f53cdfbc4ae5cb8d368ca982d257af48
rmeager <rachael.meager@gmail.com>
2020-05-22 14:18:41 +0100

    changed consumerdurables to consumption

--- data-raw/microcredit.R
+++ data-raw/microcredit.R
@@ -6,31 +6,33 @@ load("data-raw/microcredit_project_data.RData")
 
 # This preps data so that the model can be written efficiently -
 # it's not necessary but it's the best way I know to code it in STAN
-group <- c( attanasio_indicator, augsberg_indicator,
+group <- c( angelucci_indicator, attanasio_indicator, augsberg_indicator,
            banerjee_indicator, crepon_indicator)
-group <- group - 1
-consumerdurables <- c( attanasio_consumerdurables,
-                       augsberg_consumerdurables,banerjee_consumerdurables,
-                       crepon_consumerdurables)
-treatment <- c( attanasio_treatment, augsberg_treatment,
+
+consumption <- c(angelucci_consumption, attanasio_consumption,
+                       augsberg_consumption,banerjee_consumption,
+                       crepon_consumption)
+treatment <- c( angelucci_treatment, attanasio_treatment, augsberg_treatment,
                 banerjee_treatment, crepon_treatment)
 
 # Now we have to standardise any variables which are in
 # local currency units to USD PPP per fortnight in 2009 dollars
 expanded_standardiser_USD_PPP_per_fortnight <- c(
-  rep(the_consumerdurables_standardiser_USD_PPP_per_fortnight[1],
+  rep(the_consumption_standardiser_USD_PPP_per_fortnight[1],
+      length(angelucci_indicator)),
+  rep(the_consumption_standardiser_USD_PPP_per_fortnight[2],
       length(attanasio_indicator)),
-  rep(the_consumerdurables_standardiser_USD_PPP_per_fortnight[2],
+  rep(the_consumption_standardiser_USD_PPP_per_fortnight[3],
       length(augsberg_indicator)),
-  rep(the_consumerdurables_standardiser_USD_PPP_per_fortnight[3],
+  rep(the_consumption_standardiser_USD_PPP_per_fortnight[4],
       length(banerjee_indicator)),
-  rep(the_consumerdurables_standardiser_USD_PPP_per_fortnight[4],
+  rep(the_consumption_standardiser_USD_PPP_per_fortnight[5],
       length(crepon_indicator)))
 
-consumerdurables <- consumerdurables*expanded_standardiser_USD_PPP_per_fortnight
+consumption <- consumption*expanded_standardiser_USD_PPP_per_fortnight
 
 # bind everything into a data frame
-data <- data.frame(group, consumerdurables, treatment)
+data <- data.frame(group, consumption, treatment)
 
 # We gotta remove the NA values for analyses
 data <- data[complete.cases(data),]
